f82845d4c11559baff4ae97feb071a0ed7f83753
Daniel Berhane <dberhane@bmj.com>
2014-08-29 11:02:24 +0100

    puppet-labs module being used

--- modules/drupal/manifests/init.pp
+++ modules/drupal/manifests/init.pp
@@ -9,6 +9,24 @@ class drupal {
     	recurse => true
 	}
 
+	# HW The BMJ site root folder
+	file { "/opt/sites/hw":
+    	ensure => "directory",
+    	owner  => "vagrant",
+    	group  => "root",
+    	mode   => 755,
+    	recurse => true
+	}
+
+	# HW The BMJ repo
+	file { "/opt/sites/hw/drupal-site-jnl-bmj":
+    	ensure => "directory",
+    	owner  => "vagrant",
+    	group  => "root",
+    	mode   => 755,
+    	recurse => true
+	}
+
    exec { "create-drupal-db":
       unless => "/usr/bin/mysql -uroot -proot drupal",
       command => "/usr/bin/mysql -uroot -proot -e \"create database drupal; grant all on drupal.* to root@localhost identified by 'root';\"",
@@ -20,6 +38,27 @@ class drupal {
     command => "/usr/local/bin/drush -y dl drupal-7.31 --destination='/opt/sites' --drupal-project-rename='drupal' && /usr/local/bin/drush site-install -r '/opt/sites/drupal' standard --account-name=admin --account-pass='19bmjpg' --db-url='mysql://root:root@localhost/drupal'",
     require => [File["/opt/sites/drupal"], Class["drupal::drush"], Exec["create-drupal-db"]],
   }
+
+  vcsrepo { "/opt/sites/hw/drupal-site-jnl-bmj":
+  ensure   => present,
+  user => vagrant,
+  provider => git,
+  require =>  Package['git'],
+  source => "git@github.com:highwire/drupal-site-jnl-bmj.git",
+  revision => '7.x-1.x-dev',
+}
+
+
+#vcsrepo { "/opt/code/${repo}":
+#  ensure => latest,
+#  owner => $username,
+#  group => $username,
+#  provider => git,
+#  require => [ Package[ 'git' ] ],
+#  source => "git@github.com:<your account name>/<your project name>.git",
+#  revision => 'master',
+#}
+
 
 }
 
