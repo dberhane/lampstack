c3c152dafe75bdb3c8eca5314390968153c28ee5
Daniel Berhane <dberhane@bmj.com>
2014-10-15 15:37:53 +0100

    made sym link corrections

--- modules/drupal/manifests/init.pp
+++ modules/drupal/manifests/init.pp
@@ -18,74 +18,107 @@ class drupal {
     	recurse => true,
 	}
 
-# Include Drupal index.php
-file{ '/opt/sites/hw/index.php' :
+# Include Drupal settings.php
+file{ '/opt/sites/hw/settings.php' :
   ensure => present,
   owner  => "vagrant",
   group  => "vagrant",
-  source => 'puppet:///modules/drupal/index.php',
+  source => 'puppet:///modules/drupal/settings.php',
 }
+
+# # create scripts directory to be deleted     
+# file { "/opt/sites/hw/drupal-site-jnl-bmj":
+#     ensure => "directory",
+# }
+
+# # create scripts directory to be deleted     
+# file { "/opt/sites/hw/drupal-webroot":
+#     ensure => "directory",
+# }
+
+# # create scripts directory to be deleted     
+# file { "/opt/sites/hw/drupal-webroot/sites":
+#     ensure => "directory",
+# }
+
+# # create scripts directory to be deleted     
+# file { "/opt/sites/hw/drupal-webroot/sites/default":
+#     ensure => "directory",
+# }
+
+# # create scripts directory to be deleted      
+# file { "/opt/sites/hw/drupal-site-jnl-bmj/scripts":
+#     ensure => "directory",
+# }
+
+
+file { "/opt/sites/hw/files":
+   ensure => "directory",
+   owner  => "vagrant",
+   group  => "vagrant",
+   mode   => 777,    
+}
+
 
 # BMJ and HW modules, themes symlinks syntax
 file { '/opt/sites/hw/drupal-webroot/sites/default/scripts':
    ensure => 'link',
    target => '/opt/sites/hw/drupal-site-jnl-bmj/scripts',
-}
-
-file { "/opt/sites/hw/files":
-    ensure => "directory",
-    owner  => "vagrant",
-    group  => "vagrant",
-    mode   => 777,    
+   require => [vcsrepo["/opt/sites/hw/drupal-webroot"], vcsrepo["/opt/sites/hw/drupal-site-jnl-bmj"]],   
 }
 
 file { '/opt/sites/hw/drupal-webroot/sites/default/files':
    ensure => 'link',
    target => '/opt/sites/hw/files',
+   require => [vcsrepo["/opt/sites/hw/drupal-webroot"], File["/opt/sites/hw/files"]],
 }
 
 file { '/opt/sites/hw/drupal-webroot/sites/default/libraries':
    ensure => 'link',
    target => '/opt/sites/hw/drupal-site-jnl-bmj/libraries',
+   require => [vcsrepo["/opt/sites/hw/drupal-webroot"], vcsrepo["/opt/sites/hw/drupal-site-jnl-bmj"]],
 }
 
 file { '/opt/sites/hw/drupal-webroot/sites/default/modules':
    ensure => 'link',
    target => '/opt/sites/hw/drupal-site-jnl-bmj/modules',
+   require => [vcsrepo["/opt/sites/hw/drupal-webroot"], vcsrepo["/opt/sites/hw/drupal-site-jnl-bmj"]],   
 }
 
 file { '/opt/sites/hw/drupal-webroot/sites/default/themes':
    ensure => 'link',
    target => '/opt/sites/hw/drupal-site-jnl-bmj/themes',
+   require => [vcsrepo["/opt/sites/hw/drupal-webroot"], vcsrepo["/opt/sites/hw/drupal-site-jnl-bmj"]],   
 }
 
 file { '/opt/sites/hw/drupal-webroot/sites/default/settings.php':
    ensure => 'link',
    target => '/opt/sites/hw/settings.php',
+   require => [vcsrepo["/opt/sites/hw/drupal-webroot"], file["/opt/sites/hw/settings.php"]],
 }
 
-   exec { "create-drupal-db":
-      unless => "/usr/bin/mysql -uroot -proot drupal",
-      command => "/usr/bin/mysql -uroot -proot -e \"create database drupal; grant all on drupal.* to root@localhost identified by 'root';\"",
-      require => Service["mysql"],
-    }
-
-  exec { "install-drupal":
-    cwd => "/opt/sites/drupal",
-    command => "/usr/local/bin/drush -y dl drupal-7.31 --destination='/opt/sites' --drupal-project-rename='drupal' && /usr/local/bin/drush site-install -r '/opt/sites/drupal' standard --account-name=admin --account-pass='19bmjpg' --db-url='mysql://root:root@localhost/drupal'",
-    require => [File["/opt/sites/drupal"], Class["drupal::drush"], Exec["create-drupal-db"]],
-  }
-
-vcsrepo { "/opt/sites/hw/drupal-highwire":
-  ensure   => latest,
-  owner => vagrant,
-  user => root,
-  group => vagrant,
-  provider => git,
-  require =>  [Package['git'], File['/opt/sites/hw']],
-  source => "git@github.com:highwire/drupal-highwire.git",
-  revision => '7.x-1.x-stable',
+exec { "create-drupal-db":
+    unless => "/usr/bin/mysql -uroot -proot drupal",
+    command => "/usr/bin/mysql -uroot -proot -e \"create database drupal; grant all on drupal.* to root@localhost identified by 'root';\"",
+    require => Service["mysql"],
 }
+
+#   exec { "install-drupal":
+#     cwd => "/opt/sites/drupal",
+#     command => "/usr/local/bin/drush -y dl drupal-7.31 --destination='/opt/sites' --drupal-project-rename='drupal' && /usr/local/bin/drush site-install -r '/opt/sites/drupal' standard --account-name=admin --account-pass='19bmjpg' --db-url='mysql://root:root@localhost/drupal'",
+#     require => [File["/opt/sites/drupal"], Class["drupal::drush"], Exec["create-drupal-db"]],
+#   }
+
+# vcsrepo { "/opt/sites/hw/drupal-highwire":
+#   ensure   => latest,
+#   owner => vagrant,
+#   user => root,
+#   group => vagrant,
+#   provider => git,
+#   require =>  [Package['git'], File['/opt/sites/hw']],
+#   source => "git@github.com:highwire/drupal-highwire.git",
+#   revision => '7.x-1.x-stable',
+# }
 
 vcsrepo { "/opt/sites/hw/drupal-webroot":
   ensure   => latest,
@@ -93,22 +126,26 @@ vcsrepo { "/opt/sites/hw/drupal-webroot":
   user => root,
   group => vagrant,
   provider => git,
-  require =>  [Package['git'], File['/opt/sites/hw']],
-  source => "git@github.com:highwire/drupal-webroot.git",
-  revision => '7.x-1.x-stable',
+   require =>  [Package['git'], File['/opt/sites/hw']],
+   source => "git@github.com:highwire/drupal-webroot.git",
+   revision => '7.x-1.x-stable',
 }
 
 
 vcsrepo { "/opt/sites/hw/drupal-site-jnl-bmj":
-  ensure   => latest,
-  owner => vagrant,
-  user => root,
-  group => vagrant,
-  provider => git,
-  require =>  [Package['git'], File['/opt/sites/hw']],
-  source => "git@github.com:highwire/drupal-site-jnl-bmj.git",
-  revision => '7.x-1.x-dev',
+   ensure   => latest,
+   owner => vagrant,
+   user => root,
+   group => vagrant,
+   provider => git,
+   require =>  [Package['git'], File['/opt/sites/hw']],
+   source => "git@github.com:highwire/drupal-site-jnl-bmj.git",
+   revision => '7.x-1.x-dev',
 }
+
+
+
+
 
 
 }
